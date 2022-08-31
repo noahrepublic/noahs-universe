44c9dc13a3dd8719899fbcca78f031486f0bb9a1
Noah <noahdevelopsthings@gmail.com>
2022-08-31 00:48:01 -0300

    React App + Navigation Bar set up

---
+++ src/Color_Palette.js
@@ -0,0 +1,8 @@
+
+return {
+    primary: '#0D1B2A',
+    secondary: '#1B263B',
+    tertiary: '#415A77',
+    quaternary: '#778DA9',
+    text: '#E0E1DD',
+}
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+
+const Header = () => { 
+    return (
+        <header>
+            <div id='navbar'> 
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='#'>Home</a>
+                            <a href='#'>Projects</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <text id='title'>Noah's Website</text>
+        </header>
+    )
+}
+
+export default Header;
