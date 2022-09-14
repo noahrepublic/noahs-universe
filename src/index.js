7655d66582714df7a757ad31f1e1ca1ba664e73a
Noah <noahdevelopsthings@gmail.com>
2022-09-13 22:16:55 -0300

    Fixed Routing

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes, HashRouter } from "react-router-dom";
 
 import Home from "./routes/home";
 import Projects from "./routes/projects";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
